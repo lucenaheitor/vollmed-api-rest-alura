99e911c84cfe6ed9aba1f06c0939841695e84499
lucenaheitor <lucenaheitor@gmail.com>
2024-01-06 11:14:16 -0300

    implementação do spring security e autentificação de login

---
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -0,0 +1,56 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.DadosAutenticacao;
+import med.voll.api.domain.usuario.RegisterDTO;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.domain.usuario.UsuarioRepository;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register (@RequestBody @Valid RegisterDTO data){
+        if(this.repository.findByLogin(data.login())  != null) return  ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.senha());
+        Usuario newUser = new Usuario(data.login(), encryptedPassword);
+
+        this.repository.save(newUser);
+
+        return  ResponseEntity.ok().build();
+
+    }
+
+}
---
+++ src/main/java/med/voll/api/domain/usuario/RegisterDTO.java
@@ -0,0 +1,4 @@
+package med.voll.api.domain.usuario;
+
+public record RegisterDTO(String login, String senha) {
+}
